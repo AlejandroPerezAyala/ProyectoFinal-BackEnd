fdda9b94c1566820dc134e0d2358f3be626b448f
AlejandroPerezAyala <perezalejandro266@gmail.com>
2023-04-03 23:25:23 -0300

    Optimizacion de codigo en app.js

--- src/app.js
+++ src/app.js
@@ -5,20 +5,15 @@ const PORT = 8080;
 const productManager = new ProductManager();
 const app = express();
 
-app.use(express.urlencoded({extended:true}))
-app.listen(PORT, () => {`Servidor escuchando en el puerto: ${PORT}`})
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.listen(PORT, () => {console.log(`Servidor escuchando en el puerto: ${PORT}`)})
 
 app.get("/products", async (req, res) => {
     let limit = req.query.limit
-    if(!limit){
-        const productos = await productManager.getProducts()
-        return res.send(productos);
-    } else {
-        const productos = await productManager.getProducts()
-        const productLimitados = productos.slice(0,limit)
-        return res.send(productLimitados);
-    }
-    
+    const productos = await productManager.getProducts();
+    let productofiltrados = limit ? productos.slice(0,limit) : productos;
+    res.send(productofiltrados);    
 })
 
 app.get("/products/:pid", async (req, res) => {
